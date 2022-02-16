c5adbfe759b9efa9499326c7d1ada975e39c9ecb
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-02-16 23:07:53 +0300

    1

--- src/js/main.js
+++ src/js/main.js
@@ -96,8 +96,6 @@ animate();
 //js hero scripts
 const btnplay = document.querySelector('.welcome-field__button');
 const welcomefield = document.querySelector('.welcome-field');
-const ammo = document.querySelector('.ammo');
-const ammoAmount = document.querySelectorAll('.ammo__amount');
 btnplay.addEventListener('click', () => {
   welcomefield.remove();
   function createbulletimg() {
@@ -106,24 +104,21 @@ btnplay.addEventListener('click', () => {
     bulletimg.setAttribute('src', 'images/bullet.png');
     ammo.append(bulletimg);
   }
-  for(let i = 0; i <=10; i ++) {
-    setTimeout(createbulletimg, 700 * i)
+  for(let i = 0; i <=9; i ++) {
+    setTimeout(createbulletimg, 700 * i);
   }
 })
 
-//game canvas
+//game field
 
 const player = document.querySelector('#player');
 const gamefield = document.querySelector('.game-field');
+const ammo = document.querySelector('.ammo');
+const ammoAmount = document.querySelectorAll('.ammo__amount');
+const modalammoBtn = document.querySelector('.modal-ammo__btn');
 
 
-// arrowTop code = 38;
-// arrowBottom code =  40;
-// arrowLeft code = 37;
-// arrowRight code = 39;
-
-// enter charCode/code =13;
-
+//control for game
 document.addEventListener('keydown', movedirection);
 
 function movedirection(event) {
@@ -139,24 +134,122 @@ function movedirection(event) {
   if(event.key === 'd' || event.key === 'D' || event.key === 'в'|| event.key === 'В'||event.keyCode === 39) {
     player.style.left = player.offsetLeft + 15 + 'px';
   }
-  if (event.keyCode === 32) {  
+  if (event.keyCode === 32) { 
     setTimeout(createBullet, 500);
+    deleteAmmonimg();
   }  
 }
 
+//delete modal window (ammoOFF)
+modalammoBtn.addEventListener('click', () => {
+  modalammoBtn.parentNode.parentNode.removeChild(modalammoBtn.parentNode);
+})
+
+
+function deleteAmmonimg() {
+  const ammoAmount = document.querySelectorAll('.ammo__amount');
+  ammoAmount[ammoAmount.length - 1].remove();
+  if(ammoAmount.length === 1) {
+    messageAmmountOff();
+  }
+} 
+
+
+function messageAmmountOff() {
+  const newtextammo = document.createElement('div');
+  newtextammo.innerHTML = 'Пули закончились, я тебя предупреждал!';
+  newtextammo.className = 'messageAmmountOff';
+
+  const newtextammoBtn = document.createElement('img');
+  newtextammoBtn.setAttribute('src', 'images/btnimg.png');
+  newtextammoBtn.setAttribute('draggable', "false");
+  newtextammoBtn.className = 'messageAmmountBtn';
+  newtextammo.appendChild(newtextammoBtn);
+  ammo.appendChild(newtextammo);
+  newtextammoBtn.addEventListener('click', () => {
+    newtextammoBtn.parentNode.parentNode.removeChild(newtextammoBtn.parentNode);
+  })
+}
 
 //create bullet function 
 function createBullet() {
+  const ammoAmount = document.querySelectorAll('.ammo__amount');
+  if(ammoAmount.length >= 1){
   let bullet = document.createElement('div');
   bullet.className = 'bullet';
   bullet.style.top = player.offsetTop + 50 + 'px';
   bullet.style.left = player.offsetLeft + 150 + 'px';
   gamefield.appendChild(bullet);
-
-  setInterval(function(){
+  isShot(bullet);
+  let timer = setInterval(function(){
     bullet.style.left = bullet.offsetLeft + 25 + 'px';
     bullet.style.top = bullet.offsettop + 100 + 'px';
+    if(bullet.offsetLeft > gamefield.clientWidth){
+      bullet.remove();
+    }
   },50);
+  }
 }
 
+
+//random enemymodelIMG
+const enemyimgArray = [
+  'images/enemy1.png',
+  'images/enemy2.png',
+  'images/enemy3.png',
+  'images/enemy4.png',
+  'images/enemy5.png',
+  'images/enemy7.png',
+]
+const enemyIndex = randomInteger(1, enemyimgArray.length - 1);
+let enemyimgrandom = enemyimgArray[enemyIndex];
+
+//create enemy function
+function createEnemy() {
+  let enemy = document.createElement('img');
+  enemy.setAttribute('src', enemyimgrandom);
+  enemy.className = 'enemy';
+  enemy.style.top = randomEnemyTopresult + '%';
+  gamefield.appendChild(enemy);
+  
+  let timerID = setInterval(() =>{
+  enemy.style.left = enemy.offsetLeft - randomSpeedresult + 'px';
+  }, 100);
+  enemy.dataset.timer = timerID;
+}
+
+
+
+//function for random acceleration enemy model
+let randomSpeedresult = randomSpeed(5, 9);
+function randomSpeed(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+let randomEnemyTopresult = randomEnemyTop(5, 87);
+function randomEnemyTop(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+  
+  createEnemy();
+
+
+function isShot(bullet) {
+  let enemy = document.querySelector('.enemy');
+  let bulletTop = bullet.offsetTop;
+  let bulletBottom = bullet.offsetTop + bullet.offsetHeight;
+  let bulletLeft = bullet.offsetLeft;
+  let enemyTop = enemy.offsetTop;
+  let enemyBottom = enemy.offsetTop + enemy.offsetHeight;
+  let enemyLeft = enemy.offsetLeft;
+  let enemyY = enemy.offsetWidth;
+  let bulletY = bullet.offsetWidth;
+  console.log(enemyY);
+    if(bulletY === enemyY) {
+      enemy.setAttribute('src', 'images/boom.gif');
+      setTimeout(() => {
+        enemy.remove();
+      }, 1000);
+    }
+} 
 
