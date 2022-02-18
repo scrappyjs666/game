8f0ab6c42c8f7e09333d8b282e4b00038a88746b
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-02-18 23:07:52 +0300

    1

--- src/js/main.js
+++ src/js/main.js
@@ -94,58 +94,76 @@ function animate () {
 animate();
 
 //js hero scripts
-const btnplay = document.querySelector('.welcome-field__button');
+const btnwelcome = document.querySelector('.welcome-field__button');
+const gamefield = document.querySelector('.game-field');
 const welcomefield = document.querySelector('.welcome-field');
-btnplay.addEventListener('click', () => {
+const settingfield = document.querySelector('.setting__field');
+const btnsetting = document.querySelector('.setting-field__button ');
+btnwelcome.addEventListener('click', () => {
   welcomefield.remove();
-  function createbulletimg() {
-    let bulletimg = document.createElement('img');
-    bulletimg.className = 'ammo__amount';
-    bulletimg.setAttribute('src', 'images/bullet.png');
-    ammo.append(bulletimg);
-  }
+  // settingfield.style.display = 'inline';
+  createbulletimg();
   for(let i = 0; i <=9; i ++) {
     setTimeout(createbulletimg, 700 * i);
   }
 })
 
+btnsetting.addEventListener('click', () => {
+  settingfield.remove();
+  // gamefield.style.display = 'inline';
+})
+
+//game setting start
+
 //game field
 
 const player = document.querySelector('#player');
-const gamefield = document.querySelector('.game-field');
 const ammo = document.querySelector('.ammo');
 const ammoAmount = document.querySelectorAll('.ammo__amount');
 const modalammoBtn = document.querySelector('.modal-ammo__btn');
+const hitAudio = new Audio('sound/odinochnyiy-lazernyiy-vyistrel.mp3')
+let enemy = null;
+let bullet = null;
+let interval = null;
 
+//start game
 
 //control for game
 document.addEventListener('keydown', movedirection);
-
 function movedirection(event) {
   if(event.key === 'w' || event.key === 'W' || event.key === 'Ц'|| event.key === 'ц' || event.keyCode === 38) {
-    player.style.top = player.offsetTop - 15 + 'px';
+    player.style.top = player.offsetTop - 25 + 'px';
   }  
   if(event.key === 's' || event.key === 'S' || event.key === 'Ы'|| event.key === 'ы' || event.keyCode === 40) {
-    player.style.top = player.offsetTop + 15 + 'px';
+    player.style.top = player.offsetTop + 25 + 'px';
   }
   if(event.key === 'a' || event.key === 'A' || event.key === 'ф'|| event.key === 'Ф'|| event.keyCode === 37) {
-    player.style.left = player.offsetLeft - 15 + 'px';
+    player.style.left = player.offsetLeft - 25 + 'px';
   }
   if(event.key === 'd' || event.key === 'D' || event.key === 'в'|| event.key === 'В'||event.keyCode === 39) {
-    player.style.left = player.offsetLeft + 15 + 'px';
+    player.style.left = player.offsetLeft + 25 + 'px';
   }
   if (event.keyCode === 32) { 
+    hitAudio.play();
     setTimeout(createBullet, 500);
-    deleteAmmonimg();
+    // deleteAmmonimg();
   }  
 }
 
-//delete modal window (ammoOFF)
+// imgbullet field create
+function createbulletimg() {
+    let bulletimg = document.createElement('img');
+    bulletimg.className = 'ammo__amount';
+    bulletimg.setAttribute('src', 'images/bullet.png');
+    ammo.append(bulletimg);
+  }
+
+// delete modal window (ammoOFF)
 modalammoBtn.addEventListener('click', () => {
   modalammoBtn.parentNode.parentNode.removeChild(modalammoBtn.parentNode);
 })
 
-
+// gamefield lose ammo = delete ammoimg
 function deleteAmmonimg() {
   const ammoAmount = document.querySelectorAll('.ammo__amount');
   ammoAmount[ammoAmount.length - 1].remove();
@@ -153,8 +171,7 @@ function deleteAmmonimg() {
     messageAmmountOff();
   }
 } 
-
-
+// message for player ammoOFF
 function messageAmmountOff() {
   const newtextammo = document.createElement('div');
   newtextammo.innerHTML = 'Пули закончились, я тебя предупреждал!';
@@ -171,28 +188,11 @@ function messageAmmountOff() {
   })
 }
 
-//create bullet function 
-function createBullet() {
-  const ammoAmount = document.querySelectorAll('.ammo__amount');
-  if(ammoAmount.length >= 1){
-  let bullet = document.createElement('div');
-  bullet.className = 'bullet';
-  bullet.style.top = player.offsetTop + 50 + 'px';
-  bullet.style.left = player.offsetLeft + 150 + 'px';
-  gamefield.appendChild(bullet);
-  isShot(bullet);
-  let timer = setInterval(function(){
-    bullet.style.left = bullet.offsetLeft + 25 + 'px';
-    bullet.style.top = bullet.offsettop + 100 + 'px';
-    if(bullet.offsetLeft > gamefield.clientWidth){
-      bullet.remove();
-    }
-  },50);
-  }
-}
+// const ammoAmount = document.querySelectorAll('.ammo__amount');
+// if(ammoAmount.length >= 1){}
 
 
-//random enemymodelIMG
+  //random enemymodelIMG
 const enemyimgArray = [
   'images/enemy1.png',
   'images/enemy2.png',
@@ -201,55 +201,90 @@ const enemyimgArray = [
   'images/enemy5.png',
   'images/enemy7.png',
 ]
-const enemyIndex = randomInteger(1, enemyimgArray.length - 1);
-let enemyimgrandom = enemyimgArray[enemyIndex];
 
-//create enemy function
+
+//random acceleration enemy model
+function randomSpeed(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+//random enemy respawn position (top)
+function randomEnemyTop(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+//create enemy 
 function createEnemy() {
+  const enemyIndex = randomInteger(1, enemyimgArray.length - 1);
+  let enemyimgrandom = enemyimgArray[enemyIndex];
+  let randomEnemyTopresult = randomEnemyTop(5, 87);
   let enemy = document.createElement('img');
   enemy.setAttribute('src', enemyimgrandom);
   enemy.className = 'enemy';
   enemy.style.top = randomEnemyTopresult + '%';
   gamefield.appendChild(enemy);
-  
-  let timerID = setInterval(() =>{
+  enemyMove(enemy);
+}
+
+//create bullet function 
+function createBullet() {
+  let bullet = document.createElement('div');
+  bullet.className = 'bullet';
+  bullet.style.top = player.offsetTop + 50 + 'px';
+  bullet.style.left = player.offsetLeft + 150 + 'px';
+  gamefield.appendChild(bullet);
+  bulletMove(bullet);
+}
+
+//create enemyMove 
+function enemyMove(enemy){
+  let randomSpeedresult = randomSpeed(5, 9);
+  setInterval(() => {
   enemy.style.left = enemy.offsetLeft - randomSpeedresult + 'px';
-  }, 100);
-  enemy.dataset.timer = timerID;
+  }, 100); 
+  // if(enemy.offsetLeft < gamefield.clientWidth) {
+  //   enemy.remove();
+  // }
 }
 
-
-
-//function for random acceleration enemy model
-let randomSpeedresult = randomSpeed(5, 9);
-function randomSpeed(min, max) {
-  return Math.random() * (max - min) + min;
+//bulletmove and remove bullet if bullet >= gamefield
+function bulletMove(bullet){
+  let timer = setInterval(() => {
+  bullet.style.left = bullet.offsetLeft + 25 + 'px';
+  isShot(bullet);
+  if(bullet.offsetLeft > gamefield.clientWidth){
+  bullet.remove();
+  clearInterval(timer);
+    }
+  },30);
 }
-
-let randomEnemyTopresult = randomEnemyTop(5, 87);
-function randomEnemyTop(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-  
-  createEnemy();
-
 
 function isShot(bullet) {
   let enemy = document.querySelector('.enemy');
-  let bulletTop = bullet.offsetTop;
-  let bulletBottom = bullet.offsetTop + bullet.offsetHeight;
-  let bulletLeft = bullet.offsetLeft;
+  if(enemy != null) {
   let enemyTop = enemy.offsetTop;
+  let bulletTop = bullet.offsetTop;
+  
+  let bulletBottom = bullet.offsetTop + bullet.offsetHeight;
   let enemyBottom = enemy.offsetTop + enemy.offsetHeight;
-  let enemyLeft = enemy.offsetLeft;
-  let enemyY = enemy.offsetWidth;
-  let bulletY = bullet.offsetWidth;
-  console.log(enemyY);
-    if(bulletY === enemyY) {
-      enemy.setAttribute('src', 'images/boom.gif');
-      setTimeout(() => {
-        enemy.remove();
-      }, 1000);
-    }
-} 
 
+  let bulletLeft = bullet.offsetLeft + 2;
+  let enemyLeft = enemy.offsetLeft + 2;
+
+  if(bulletBottom <= enemyBottom &&  bulletLeft + bullet.offsetWidth >= enemyLeft && bulletLeft <= enemyLeft + enemy.offsetWidth && bulletTop >= enemyTop) {
+    enemy.setAttribute('src', 'images/boom.gif');
+    setTimeout(() => {
+    enemy.remove();
+    }, 1000);
+    bullet.remove();
+    clearInterval(interval);
+  }
+  }
+}
+
+gamefield.addEventListener('click', function(){
+  createEnemy();
+  setInterval(() => {
+  createEnemy();
+  }, 5000);
+})
