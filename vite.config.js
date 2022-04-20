59323ee79e0b810c76c814d70aae04eefa024e2d
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-04-20 21:48:45 +0300

    Key changes added, project structure changed

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  root: 'src',
+  base: '/game/dist/',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        'index': resolve(__dirname, 'src/index.html'),
+      }
+    }
+  }
+})
+
